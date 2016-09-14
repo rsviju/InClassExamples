fc8fd417cdd7433dade839ad7ce8cbd79a3eb4bf
Eric Torunski <etorunski@gmail.com>
2016-09-13 21:59:15 -0400

    New stuff that I've added

--- app/src/main/java/com/example/etorunski/inclassexamples/MainActivity.java
+++ app/src/main/java/com/example/etorunski/inclassexamples/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.etorunski.inclassexamples;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = null;
+      // b.setOnClickListener( click -> Log.i("Click", "Click"));
+
+        b.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Log.i("Hi", "There");
+            }
+        });
+
+
     }
 }
